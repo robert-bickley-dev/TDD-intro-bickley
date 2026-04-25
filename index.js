4c1c023fc5da7b934599a63e7b288b39c4f41799
Rob Bickley <robert.john.bickley@gmail.com>
2026-04-25 12:04:44 -0400

    Add sumDouble function

--- index.js
+++ index.js
@@ -12,7 +12,11 @@
  * sumDouble(5, 5); // 20
  */
 export function sumDouble(num1, num2) {
-  /* TODO */
+  const sum = num1 + num2;
+  if (num1 === num2) {
+    return sum * 2;
+  }
+  return sum;
 }
 
 /**
